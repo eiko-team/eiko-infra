346ecee6dfea47cb0cd3b1a172b020390ab953d0
Tom Moulard <tom.moulard@epita.fr>
2019-09-05 18:09:54 +0200

    terraform: adding some simple variables

---
+++ variable.tf
@@ -0,0 +1,43 @@
+variable "project" {
+    default = "poetic-hexagon-252009"
+}
+
+variable "region" {
+    default = "europe-west"
+}
+
+variable "cluster_name" {
+    default = "eikos-cluster"
+}
+
+variable "cluster_location" {
+    default = "europe-west1-b"
+}
+
+variable "cluster_k8s_version" {
+    default = "1.13.7-gke.24"
+}
+
+variable "initial_node_count" {
+    default = 2
+}
+
+variable "autoscaling_min_node_count" {
+    default = 1
+}
+
+variable "autoscaling_max_node_count" {
+    default = 2
+}
+
+variable "disk_size_gb" {
+    default = 50
+}
+
+variable "disk_type" {
+    default = "pd-standard"
+}
+
+variable "machine_type" {
+    default = "n1-standard-1"
+}
